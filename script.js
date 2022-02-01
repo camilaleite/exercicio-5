969234f2a7236361bd9e7d54ae5e0dc7f9c576ee
camilaleite <camilalenas_@hotmail.com>
2022-02-01 13:00:12 -0300

    subindo ex 5

---
+++ script.js
@@ -0,0 +1,24 @@
+ // Crie um array que receba 5 itens e exiba no console.
+let itens = ["estojo", "lápis","borracha","caneta","papel"]
+console.log(itens)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+let adicionar = itens.unshift('régua')
+
+// Utilize um método para remover o último nome do array.
+let ultimo = itens.pop()
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+let adicionarmais = itens.push('agenda','lapiseira')
+
+// Utilize um método para remover o primeiro nome do array.
+let nomesCachorro = ["teco", "laila","lesse"]
+console.log(nomesCachorro)
+let primeiro = nomesCachorro.shift ()
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// const numbers = [7,5,6,3,8,9,2,1,4]
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+let ordenar = numbers.sort()
+console.log(numbers)
